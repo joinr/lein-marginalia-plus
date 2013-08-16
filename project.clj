0386366fd380d4697a363148e70e15a03298501d
Tom <tom@demo.com>
2013-08-16 04:59:12 -0400

    Renamed

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject lein-marg-ext "0.8.0-SNAPSHOT"
-  :description "A set of extensions to marginalia and lein-marg to default to an optional vertical layout with
-                a much more prose-friendly display.  The marginalia layout can be specified with a
-                new command-line option, -l --layout, as either \"vertical\" or \"parallel\" ."
+(defproject lein-marginalia-plus "0.8.0-SNAPSHOT"
+  :description "A set of extensions to marginalia and lein-marginalia to allow an optional vertical layout with
+                a much more prose-friendly display.  A new command is added, lein-marg-plus, which allows a marginalia
+                layout to be specified with a new command-line option, -l --layout, as either \"vertical\"
+                or \"parallel\" ."
   :url "None"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
